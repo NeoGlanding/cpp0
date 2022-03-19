7f19b7d51d0d77f4a327bf462364f6860fecd2c0
NeoGlanding <neoglanding@protonmail.com>
2022-03-19 21:37:49 +0700

    main commit

---
+++ Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>;
+
+using namespace std;
+
+int main()
+{
+
+    string characterName = "Ihsan";
+    int characterAge = 20;
+
+    // Data Types
+    char grade = 'A';
+    string academyName = "Sekolah Alternatif";
+    bool sekolahAlternatifBagus = true;
+    int tahunBerdiriSekolahAlternatif = 2020;
+    double skorSekolahAlternatif = 9.8;
+
+    cout << "Nama sekolahnya adalah " << academyName << endl;
+    cout << "Tahun berdiri sekolah alternatif adalah " << tahunBerdiriSekolahAlternatif << endl;
+    cout << "Skor Sekolah Alternatif adalah " << skorSekolahAlternatif << endl;
+
+    
+
+
+    // cout << "Hello World" << endl;
+    // cout << "My name is " << characterName << endl;
+    return 0;
+}
